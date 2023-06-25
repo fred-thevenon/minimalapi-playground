0016b32c2e89d7acce5d8fbc4eac5cb2ecff52a0
Frédéric THEVENON <frederic.thevenon@protonmail.com>
2023-06-25 18:02:57 +0200

    feat: add swagger mapping

--- Playground.Api/Program.cs
+++ Playground.Api/Program.cs
@@ -1,6 +1,27 @@
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.MapType<WeatherForecastUpload>(() => new OpenApiSchema()
+    {
+        Type = "object",
+        Properties = new Dictionary<string, OpenApiSchema>()
+        {
+            {
+                "file",
+                new OpenApiSchema { Type = "string", Format = "binary", Required = new HashSet<string> { "true" }, Nullable = false}
+            },
+            {
+                "date",
+                new OpenApiSchema { Type = "string", Format = "date-time", Required = new HashSet<string> { "true" },Nullable = false }
+            }
+        }
+    });
+});
 
 var app = builder.Build();
 app.UseSwagger();
@@ -26,8 +47,40 @@ app.MapGet("/weatherforecast", () =>
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
+app.MapPost("/weatherforecast/upload",
+        (WeatherForecastUpload weatherForecastUpload) =>
+        {
+            Debug.WriteLine(weatherForecastUpload.Date);
+            Debug.WriteLine(weatherForecastUpload.File.Name);
+            Debug.WriteLine(weatherForecastUpload.File.FileName);
+            Debug.WriteLine(weatherForecastUpload.File.ContentType);
+
+            return Results.Ok("citron");
+        })
+    .Produces<string>(200)
+    .Accepts<WeatherForecastUpload>("multipart/form-data");
 
 app.Run();
+
+internal class WeatherForecastUpload
+{
+    public IFormFile File { get; set; }
+    public DateTime Date { get; set; }
+
+    public static async ValueTask<WeatherForecastUpload?> BindAsync(HttpContext httpContext, ParameterInfo parameter)
+    {
+        var form = await httpContext.Request.ReadFormAsync();
+        var file = form.Files["file"]!;
+        var date = DateTime.Now;
+
+        return
+            new WeatherForecastUpload()
+            {
+                File = file,
+                Date = date
+            };
+    }
+}
 
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
 {
---
+++ Playground.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WeatherService;
+
+Console.WriteLine("Hello, World!");
+
+using var httpClient = new HttpClient();
+var weatherClient = new WeatherClient("https://localhost:7075/", httpClient);
+var result = await weatherClient.UploadAsync(new FileParameter(File.OpenRead(@"C:\Users\FrédéricTHEVENON\Downloads\adhesion_649041e3c67cd16b1c0377fc.pdf")), DateTimeOffset.Now.DateTime);
+Console.WriteLine(result);
